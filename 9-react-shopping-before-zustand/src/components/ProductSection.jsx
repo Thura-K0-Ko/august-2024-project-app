47f69e9a47aebdd4124fb841e8a26fca4172bee0
Thura-K0-Ko <Thura-KO-Ko>
2024-08-30 04:06:27 +0630

    basic-todo-ui-build

--- 9-react-shopping-before-zustand/src/components/ProductSection.jsx
+++ 9-react-shopping-before-zustand/src/components/ProductSection.jsx
@@ -4,14 +4,13 @@ import Container from "./Container";
 import products from "../data/Product";
 
 const ProductSection = () => {
-
   return (
     <section className=" px-5">
       <Container>
         <p className=" mb-2 font-semibold text-sm text-gray-500 ">
           Avaliable Product List
         </p>
-        <div className=" grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        <div className=" grid  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
